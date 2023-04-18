7eeb0222ba3d5d0437c27ca204d527d37d1c3a87
bilel86 <bileljaouadi86@gmail.com>
2023-04-19 00:10:26 +0100

    UPD login.py

--- login.py
+++ login.py
@@ -7,11 +7,12 @@ Write.Print(" write username & password \n\n", Colors.red_to_white, interval=0.1
 print(Box.DoubleCube("Login "))
 
 while True:
-    username= Write.Input('write username: \n', Colors.black, interval=0.1)
-    password= Write.Input('write password: \n\n', Colors.black,interval=0.1)
+    username= Write.Input('write username: \n', Colors.white, interval=0.1)
+    password= Write.Input('write password: \n\n', Colors.white,interval=0.1)
     if username == 'bilel' and password =='bilel1234':
         Write.Print("Succeful ..\n", Colors.green,interval=0.1)
-        input('\n press any key to exit ...')
+        
     else:
         Write.Print("Error try again \n\n", Colors.red, interval=0.1)
-
+    input('\n press any key to exit ...')
+ 
